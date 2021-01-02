dbcf7e79c44cd7365fafa0d3ad6e90c36dd03248
seekr123 <50204829+seekr123@users.noreply.github.com>
2021-01-02 13:06:51 +0530

    Add files via upload

---
+++ class38/asynchronousBallMovement/js/player.js
@@ -0,0 +1,30 @@
+class Player{
+    constructor(){
+        this.index=null;
+        this.distance=0;
+        this.name=null;
+
+    }
+    getCount(){
+       database.ref('playerCount').on('value',(data)=>{
+           playerCount=data.val();
+       });
+    }
+    updateCount(count){
+       database.ref('/').update({
+           playerCount:count
+       })
+    }
+    update(){
+        var playerIndex='players/player'+this.index;
+        database.ref(playerIndex).set({
+            name:this.name,
+            distance:this.distance
+        })
+    }
+    static getPlayerInfo(){
+        database.ref('players').on('value',(data)=>{
+           allPlayers=data.val();
+        });
+    }
+}
